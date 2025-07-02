ff6d9df1821e6630531eb6fba7be14435777847f
k4udeepak <p4udeepak@gmail.com>
2025-07-02 23:49:31 +0530

    MyFirstCommit

---
+++ MyJavaApp/src/com/deepak/JavaThread.java
@@ -0,0 +1,11 @@
+package com.deepak;
+
+public class JavaThread extends Thread{
+	public void run() {
+		System.out.println("Thread");
+	}
+public static void main(String[] args) {
+	JavaThread th1=new JavaThread();
+	th1.start();
+}
+}

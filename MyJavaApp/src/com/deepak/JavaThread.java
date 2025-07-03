c8623a6be30f1911c370e400221493a0d638ffc7
k4udeepak <p4udeepak@gmail.com>
2025-07-03 12:39:48 +0530

    Minor change

--- MyJavaApp/src/com/deepak/JavaThread.java
+++ MyJavaApp/src/com/deepak/JavaThread.java
@@ -3,6 +3,7 @@ package com.deepak;
 public class JavaThread extends Thread{
 	public void run() {
 		System.out.println("Thread");
+		System.out.println("Running");
 	}
 public static void main(String[] args) {
 	JavaThread th1=new JavaThread();

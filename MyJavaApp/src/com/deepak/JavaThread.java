46decf5167f656b4c8d2e031050091dc54d5f2ea
k4udeepak <p4udeepak@gmail.com>
2025-07-03 15:50:06 +0530

    Added stopping

--- MyJavaApp/src/com/deepak/JavaThread.java
+++ MyJavaApp/src/com/deepak/JavaThread.java
@@ -4,6 +4,7 @@ public class JavaThread extends Thread{
 	public void run() {
 		System.out.println("Thread");
 		System.out.println("Running");
+		System.out.println("Stopping");
 	}
 public static void main(String[] args) {
 	JavaThread th1=new JavaThread();
